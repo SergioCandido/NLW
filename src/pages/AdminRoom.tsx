b2e27a8a195ff0b4b4cbc5e90db95116b169d29d
Sergio_candido <seergincaandido@gmail.com>
2021-06-24 12:03:49 -0300

    finaliza pagino do admin e corrige bug no bd

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -15,7 +15,7 @@ type RoomParams = {
     id: string;
 }
 
-export function Room() {
+export function AdminRoom() {
     const { user } = useAuth()
     const params = useParams<RoomParams>()
     const [newQuestion, setNewQuestion] = useState('')
@@ -35,7 +35,7 @@ export function Room() {
         }
 
         const question = {
-            content: newQuestion,
+            question: newQuestion,
             author: {
                 name: user.name,
                 avatar: user.avatar
@@ -55,7 +55,10 @@ export function Room() {
             <header>
                 <div className="content">
                     <img src={logoImg} alt="Letmeask" />
-                    <RoomCode code={roomId} />
+                    <div>
+                        <RoomCode code={roomId} />
+                        <Button isOutlined>Encerrar sala</Button>
+                    </div>
                 </div>
             </header>
 
@@ -65,25 +68,6 @@ export function Room() {
                     {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
                 </div>
 
-                <form onSubmit={handleSendQuestion}>
-                    <textarea
-                        placeholder="O que você quer perguntar?"
-                        onChange={event => setNewQuestion(event.target.value)}
-                        value={newQuestion}
-                    />
-
-                    <div className="form-footer">
-                        {user ? (
-                            <div className="user-info">
-                                <img src={user.avatar} alt={user.name} />
-                                <span>{user.name}</span>
-                            </div>
-                        ) : (
-                            <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
-                        )}
-                        <Button type="submit" disabled={!user}>Enviar pergunta</Button>
-                    </div>
-                </form>
                 <div className="question-list">
                     {questions.map(question => {
                         return (
